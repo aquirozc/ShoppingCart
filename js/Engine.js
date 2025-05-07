169ff80e7cbfe97a025a417eb2059e507574a832
Alejandro Quiroz Carmona <alejandroqc03@outlook.com>
2025-05-07 07:35:02 -0600

    Cart Items persist after page reload

--- js/Engine.js
+++ js/Engine.js
@@ -1,36 +1,17 @@
 class CartController{
 
     cart = document.getElementById("lista-carrito").querySelector("tbody");
-    freq = new Map();
+    freq = new Map(Object.entries(JSON.parse(localStorage.getItem('cart') ?? "{}")));
     
     constructor(){
         document.querySelectorAll(".agregar-carrito")
                 .forEach(n => n.addEventListener('click',e => this.PushItemToCart(e)));
         document.getElementById("vaciar-carrito")
                 .addEventListener('click', e => this.WipeCart(e));
+        this.DrawCart();
     }
 
-    ModifyItemCount(id, event){
-        this.freq.get(id).count = parseInt(event.target.value);
-    }
-
-    PushItemToCart(event){
-        event.preventDefault();
-
-        let root = event.target.parentElement.parentElement;
-        let id = event.target.getAttribute('data-id');
-
-        let item  = {
-            name : root.querySelector(".info-card h4").innerText,
-            img : root.querySelector(".imagen-curso").src,
-            price : root.querySelector(".precio span").innerText,
-        };
-
-        this.freq.set(id, {
-            ...item,
-            count: parseInt((this.freq.get(id)?.count || 0) + 1)
-        });
-        
+    DrawCart(){
         this.cart.innerHTML = "";
         
         this.freq.forEach((value, key) => {
@@ -50,24 +31,57 @@ class CartController{
             `;
 
             row.querySelector("input[type='number']")
-                .addEventListener('change', e => this.ModifyItemCount(id,e));
+                .addEventListener('change', e => this.ModifyItemCount(key,e));
 
             row.querySelector("input[type='button']")
-                .addEventListener('click', e => this.RemoveItemFromCart(id,e));
+                .addEventListener('click', e => this.RemoveItemFromCart(key,e));
 
             this.cart.appendChild(row);
         });
     }
 
+    ModifyItemCount(id, event){
+        this.freq.get(id).count = parseInt(event.target.value);
+        this.SaveCart();
+    }
+
+    PushItemToCart(event){
+        event.preventDefault();
+
+        let root = event.target.parentElement.parentElement;
+        let id = event.target.getAttribute('data-id');
+
+        let item  = {
+            name : root.querySelector(".info-card h4").innerText,
+            img : root.querySelector(".imagen-curso").src,
+            price : root.querySelector(".precio span").innerText,
+        };
+
+        this.freq.set(id, {
+            ...item,
+            count: parseInt((this.freq.get(id)?.count || 0) + 1)
+        });
+
+        this.SaveCart();
+        this.DrawCart();
+        
+    }
+
     RemoveItemFromCart(id,event){
         this.freq.delete(id);
         event.target.parentElement.parentElement.remove();
+        this.SaveCart();
+    }
+
+    SaveCart(){
+        localStorage.setItem('cart',JSON.stringify(Object.fromEntries(this.freq)));
     }
 
     WipeCart(event){
         event.preventDefault();
         this.cart.innerHTML="";
         this.freq.clear();
+        this.SaveCart();
     }
 
 }
